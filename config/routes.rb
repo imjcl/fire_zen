65776eaf03f16425900275db63ee766c383d603b
James Liu <jaycliu@gmail.com>
2014-08-10 15:47:44 -0700

    change root path to zen#index

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'zen' => 'zen#index', as: 'zen'
+  root 'zen#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
