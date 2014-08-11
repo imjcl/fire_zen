67865d2c57f71b76230fe89c9d2a648d8e68d074
James Liu <jaycliu@gmail.com>
2014-08-10 17:37:40 -0700

    create interaction with firebase using github service

--- app/services/zen_services.rb
+++ app/services/zen_services.rb
@@ -1,7 +1,9 @@
 class ZenServices
   def self.get_zen
     base_uri = 'https://api.github.com/zen'
-    return RestClient.get base_uri if (ENV["CID"].nil? && ENV["CSECRET"].nil?)
-    return RestClient.get base_uri + "?client_id=#{ENV["CID"]}&client_secret=#{ENV["CSECRET"]}"
+    
+    FirebaseServices.push_zen RestClient.get base_uri if (ENV["CID"].nil? && ENV["CSECRET"].nil?)
+
+    FirebaseServices.push_zen RestClient.get base_uri + "?client_id=#{ENV["CID"]}&client_secret=#{ENV["CSECRET"]}"
   end
 end
