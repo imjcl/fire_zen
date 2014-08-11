12975b0612fcf5e1705dde05e600db2403880d2c
James Liu <jaycliu@gmail.com>
2014-08-10 17:07:34 -0700

    add handling for whether or not we have oauth credentials for bypassing anon rate limits

--- app/services/zen_services.rb
+++ app/services/zen_services.rb
@@ -1,5 +1,7 @@
 class ZenServices
   def self.get_zen
-    RestClient.get 'https://api.github.com/zen'
+    base_uri = 'https://api.github.com/zen'
+    return RestClient.get base_uri if (ENV["CID"].nil? && ENV["CSECRET"].nil?)
+    return RestClient.get base_uri + "?client_id=#{ENV["CID"]}&client_secret=#{ENV["CSECRET"]}"
   end
 end
