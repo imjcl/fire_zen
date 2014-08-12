289fce5b4262af98f2f61f436955c49c12b7f28d
James Liu <jaycliu@gmail.com>
2014-08-12 12:03:36 -0700

    refactor zen_services such that it is now called by firezen_handler

--- app/services/zen_services.rb
+++ app/services/zen_services.rb
@@ -2,8 +2,8 @@ class ZenServices
   def self.get_zen
     base_uri = 'https://api.github.com/zen'
     
-    FirebaseServices.push_zen RestClient.get base_uri if (ENV["CID"].nil? && ENV["CSECRET"].nil?)
+    RestClient.get base_uri if (ENV["CID"].nil? && ENV["CSECRET"].nil?)
 
-    FirebaseServices.push_zen RestClient.get base_uri + "?client_id=#{ENV["CID"]}&client_secret=#{ENV["CSECRET"]}"
+    RestClient.get base_uri + "?client_id=#{ENV["CID"]}&client_secret=#{ENV["CSECRET"]}"
   end
 end
