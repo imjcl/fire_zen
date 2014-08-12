27502359c22fbdc7d3d1cbee1eab182a2f1373f1
James Liu <jaycliu@gmail.com>
2014-08-12 12:03:01 -0700

    change handler to actually perform functions as a service manager

--- app/services/firezen_handler.rb
+++ app/services/firezen_handler.rb
@@ -1,5 +1,11 @@
 class FirezenHandler
-  def self.say_hi
-    'Hi'
+  def self.send_fire_zen
+    quote = ZenServices.get_zen
+    FirebaseServices.push_zen quote
+  end
+
+  def self.get_fire_zen
+    firebase = Firebase::Client.new(ENV['FIREBASE'])
+    firebase.get("zen")
   end
 end
