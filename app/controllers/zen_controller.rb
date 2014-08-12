27232fe136fa58185000cdeabf30423c99abb04b
James Liu <jaycliu@gmail.com>
2014-08-12 12:03:15 -0700

    change zen controller to utilize service manager

--- app/controllers/zen_controller.rb
+++ app/controllers/zen_controller.rb
@@ -1,5 +1,10 @@
 class ZenController < ApplicationController
   def index
-    @zen = ZenServices::get_zen
+    @zen = FirezenHandler::get_fire_zen
+  end
+
+  def get_new_zen
+    FirezenHandler.send_fire_zen
+    redirect_to root_url
   end
 end
