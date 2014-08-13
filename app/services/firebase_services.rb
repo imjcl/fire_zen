7353da97580fbaad3d2973c0da44f66a025bf198
James Liu <jaycliu@gmail.com>
2014-08-13 16:26:12 -0700

    add handling for likes and dislikes counter from initiation and add mechanism to remove duplicate keys

--- app/services/firebase_services.rb
+++ app/services/firebase_services.rb
@@ -1,6 +1,13 @@
 class FirebaseServices
   def self.push_zen quote
     firebase = Firebase::Client.new(ENV['FIREBASE'])
-    response = firebase.push("zen", { quote: quote })
+    response = firebase.push("zen", { quote: quote, likes: 0, dislikes: 0 })
+  end
+
+  def self.remove_zen_keys keys
+    firebase = Firebase::Client.new(ENV['FIREBASE'])
+    keys.each do |key|
+      firebase.delete("zen/#{key}")
+    end
   end
 end
