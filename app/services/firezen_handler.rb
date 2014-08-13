531fde8a05ca9928b9c783e152c217e6b8c469c8
James Liu <jaycliu@gmail.com>
2014-08-13 16:26:47 -0700

    prevent duplicates from being displayed, right now they are removed on page refresh

--- app/services/firezen_handler.rb
+++ app/services/firezen_handler.rb
@@ -6,6 +6,16 @@ class FirezenHandler
 
   def self.get_fire_zen
     firebase = Firebase::Client.new(ENV['FIREBASE'])
-    firebase.get("zen").body.sort_by { |z| z[1]["likes"]}.reverse
+    get_unique_zens firebase.get("zen").body.sort_by { |z| z[1]["likes"]}.reverse
   end
+
+  def self.get_unique_zens zens
+    selects = zens.uniq { |z| z[1]["quote"] }
+    raw = zens.flatten.select { |z| z.is_a? String }
+    uniques = selects.flatten.select { |s| s.is_a? String }
+
+    FirebaseServices::remove_zen_keys (raw - uniques)
+
+    selects
+  end  
 end
