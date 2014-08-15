0e52360d4c0dbd82cf55767e479f73f0479d4908
James Liu <jaycliu@gmail.com>
2014-08-14 23:49:52 -0700

    rename get new zen method to from_new_zen

--- app/controllers/zen_controller.rb
+++ app/controllers/zen_controller.rb
@@ -3,10 +3,8 @@ class ZenController < ApplicationController
     @zen = FirezenHandler::get_fire_zen
   end
 
-  def get_new_zen
+  def from_new_zen
     FirezenHandler.send_fire_zen
-    respond_to do |format|
-      format.html
     end
   end
 end
