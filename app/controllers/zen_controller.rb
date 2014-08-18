4e29e49dabf7642d19dcf249fb1fe852d514c959
James Liu <jaycliu@gmail.com>
2014-08-18 12:42:48 -0700

    add functionality for submitting zen, anonymously right now

--- app/controllers/zen_controller.rb
+++ app/controllers/zen_controller.rb
@@ -6,4 +6,8 @@ class ZenController < ApplicationController
   def from_new_zen
     @is_duplicate = FirezenHandler.send_fire_zen
   end
+
+  def submit_zen
+    @is_duplicate = FirezenHandler.submit_fire_zen params[:submit_zen]
+  end
 end
